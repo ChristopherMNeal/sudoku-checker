13a7995087f058555c8d53b0a58504f4948f2d8a
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-09 14:03:54 -0800

    create second test for Board()

--- __tests__/board.test.js
+++ __tests__/board.test.js
@@ -3,7 +3,11 @@ import Board from '../src/board.js';
 describe('Board', () => {
 
   test('should correctly make an sudoku row object with an array of 9 numbers', () => {
-    const board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    const board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    expect(board.row1).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+  test('should sort the array by number', () => {
+    const board = new Board([9, 8, 7, 6, 5, 4, 3, 2, 1]);
     expect(board.row1).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 });
