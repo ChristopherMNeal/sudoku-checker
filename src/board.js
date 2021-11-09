7206fc924189b46ccb42a0e09643baa9d3fb0c09
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-09 13:56:49 -0800

    pass first test for Board()

---
+++ src/board.js
@@ -0,0 +1,13 @@
+export default function Board (row1){
+  this.row1 = row1;
+  // this.row2 = row2;
+  // this.row3 = row3;
+  // this.row4 = row4;
+  // this.row5 = row5;
+  // this.row6 = row6;
+  // this.row7 = row7;
+  // this.row8 = row8;
+  // this.row9 = row9;
+}
+
+// row2, row3, row4, row5, row6, row7, row8, row9
