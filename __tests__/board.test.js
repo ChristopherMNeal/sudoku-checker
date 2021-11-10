6e8665a61b978d2bcb3c91150b1970809672449d
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-09 16:40:39 -0800

    pass fifth test for Board()
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>
    Co-authored-by: Kim Brannian <brannian@gmail.com>

--- __tests__/board.test.js
+++ __tests__/board.test.js
@@ -15,12 +15,16 @@ let reusableBoard;
     // const board = new Board([9, 8, 7, 6, 5, 4, 3, 2, 1]);
     expect(reusableBoard.areThereDuplicates(reusableBoard.rows[1])).toEqual(false);
   });
-  test('should return true if the object does contain duplicate numbers', () => {
+  test('should return true if the row object does contain duplicate numbers', () => {
     // const board = new Board([9, 8, 7, 6, 5, 4, 3, 2, 2]);
     expect(reusableBoard.areThereDuplicates(reusableBoard.rows[9])).toEqual(true);
   });
   test('should return an array to represent the first column of the board object', () => {
     reusableBoard.makeMeSomeColumns();
     expect(reusableBoard.columns[1]).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+  test('should return true if the column object does contain duplicate numbers', () => {
+    reusableBoard.makeMeSomeColumns();
+    expect(reusableBoard.areThereDuplicates(reusableBoard.columns[9])).toEqual(true);
   });
 });
